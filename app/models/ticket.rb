f11d3317013e0c2df300714658a66766266df1d8
mkangia <manish.kangia@vinsol.com>
2013-03-16 11:25:39 +0530

    spacing fixed

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,17 +6,17 @@ class Ticket < ActiveRecord::Base
   belongs_to :creator, :class_name => "User", :foreign_key => "user_id"
 
   def self.search(search, search_by)
-  	if search
-  		if(search_by == "all")
-  		  find(:all, :conditions => ["title like ? Or description like ?", "%#{search}%", "%#{search}%"])
-  		elsif search_by == "project"
-  		  @project = Project.where(:name => search)[0]
+    if search
+      if(search_by == "all")
+        find(:all, :conditions => ["title like ? Or description like ?", "%#{search}%", "%#{search}%"])
+      elsif search_by == "project"
+        @project = Project.where(:name => search)[0]
         @project ? find(:all, :conditions => ["project_id = ?", @project.id]) : []
-  		elsif search_by
+      elsif search_by
         find(:all, :condition => ["#{search_by} like ?", "#{search}"])
-  	  else
-  		find(:all)
-  	  end
+      else
+      find(:all)
+      end
     end
   end
 end
