cb92de67da6a3925ad31817905cab8c792b7ce3a
mkangia <manish.kangia@vinsol.com>
2013-03-29 14:12:53 +0530

    I18n

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 LightHouse::Application.initialize!
+Delayed::Job.attr_accessible :priority, :payload_object, :handler, :run_at, :failed_at, :queue
